84708292ee45f48599b5e836bbb1fd69873545ef
Damian Van de Kauter <44820440+vdkdamian@users.noreply.github.com>
2026-01-29 12:05:27 +0100

    Rename Articlable protocol properties for clarity
    Renamed properties in the Articlable protocol and its extension from underscored versions (e.g., _itemId) to more conventional names (e.g., itemId) for improved readability and consistency.

--- Sources/FunicoCore/Backend/Protocols/Articlable.swift
+++ Sources/FunicoCore/Backend/Protocols/Articlable.swift
@@ -10,18 +10,18 @@ public protocol Articlable {
     typealias RawPart = Part.APIValue
     typealias RawQuality = Quality.APIValue
     
-    var _itemId: String { get }
-    var _part: RawPart? { get }
-    var _quality: RawQuality? { get }
-    var _length: Float? { get }
+    var itemId: String { get }
+    var part: RawPart? { get }
+    var quality: RawQuality? { get }
+    var length: Float? { get }
 }
 
 public extension Articlable {
     
     var article: Article {
-        let part: Part? = _part.flatMap({ try? Part(apiValue: $0) })
-        let quality: Quality? = _quality.flatMap({ try? Quality(apiValue: $0) })
+        let part: Part? = part.flatMap({ try? Part(apiValue: $0) })
+        let quality: Quality? = quality.flatMap({ try? Quality(apiValue: $0) })
         
-        return Article(_itemId, part: part, quality: quality, length: _length)
+        return Article(itemId, part: part, quality: quality, length: length)
     }
 }
